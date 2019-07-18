d84757a949edaf07bce17e0986897fc375390924
Scarlett Rogers <scarlettrgrs@gmail.com>
2019-07-19 09:27:10 +1000

    Week 6/7 mixture of assignment and dataset

---
+++ week6/assignment.py
@@ -0,0 +1,239 @@
+import argparse
+import logging
+import time
+
+import cv2
+import numpy as np
+
+import common
+from estimator import TfPoseEstimator
+from networks import get_graph_path, model_wh
+
+import platform
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+logger = logging.getLogger('TfPoseEstimator-WebCam')
+logger.setLevel(logging.CRITICAL)
+ch = logging.StreamHandler()
+ch.setLevel(logging.CRITICAL)
+formatter = logging.Formatter(
+    '[%(asctime)s] [%(name)s] [%(levelname)s] %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+fps_time = 0
+
+#
+POSE_COCO_BODY_PARTS = {
+    0: "Nose",
+    1: "Neck",
+    2: "RShoulder",
+    3: "RElbow",
+    4: "RWrist",
+    5: "LShoulder",
+    6: "LElbow",
+    7: "LWrist",
+    8: "RHip",
+    9: "RKnee",
+    10: "RAnkle",
+    11: "LHip",
+    12: "LKnee",
+    13: "LAnkle",
+    14: "REye",
+    15: "LEye",
+    16: "REar",
+    17: "LEar",
+    18: "Background",
+}
+
+
+
+# call this when a taxi is being hailed!
+def hail_taxi(img):
+    print("Someone is hailing a taxi!")
+    cv2.putText(img, "TAXI!",
+                    (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (94, 218, 255), 2)
+    cv2.putText(img, platform.uname().node,
+                    (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
+
+if __name__ == '__main__':
+    # arguements to your program
+    parser = argparse.ArgumentParser(
+        description='tf-pose-estimation realtime webcam')
+    parser.add_argument('--camera', type=int, default=0)
+    parser.add_argument('--zoom', type=float, default=1.0)
+    parser.add_argument(
+        '--resolution',
+        type=str,
+        default='432x368',
+        help='network input resolution. default=432x368')
+    parser.add_argument(
+        '--model',
+        type=str,
+        default='mobilenet_thin',
+        help='cmu / mobilenet_thin')
+    parser.add_argument(
+        '--show-process',
+        type=bool,
+        default=False,
+        help='for debug purpose, if enabled, speed for inference is dropped.')
+    args = parser.parse_args()
+
+    w, h = model_wh(args.resolution)
+    e = TfPoseEstimator(get_graph_path(args.model), target_size=(w, h))
+    camera = cv2.VideoCapture(args.camera)
+    ret_val, image = camera.read()
+    
+    print("**** CTRL+C to exit ****")
+    while True:
+        # get image form the camera
+        ret_val, image = camera.read()
+        # boilerplate
+        canvas = np.zeros_like(image)
+        img_scaled = cv2.resize(
+            image,
+            None,
+            fx=args.zoom,
+            fy=args.zoom,
+            interpolation=cv2.INTER_LINEAR)
+        dx = (canvas.shape[1] - img_scaled.shape[1]) // 2
+        dy = (canvas.shape[0] - img_scaled.shape[0]) // 2
+        canvas[dy:dy + img_scaled.shape[0], dx:
+               dx + img_scaled.shape[1]] = img_scaled
+        image = canvas
+        # feed image into the neural network
+        humans = e.inference(image)  # list of humans
+        for id, human in enumerate(humans):
+
+            # TODO ensure it only does this when someone is hailing a taxi.
+            # That is, an arm is above their head.
+
+            # (POSE_COCO_BODY_PARTS[k], v.x, v.y) for k,v in human.body_parts.items()
+        
+        #Evidence of my journey to the answer/general stupidity doing this - Scarlett
+        #I JUST WANT THE Y VALUUUUEEEE
+
+            # if (POSE_COCO_BODY_PARTS[4], v.y) <= (POSE_COCO_BODY_PARTS[0], v.y) for v in human.body_parts.items():
+            # (POSE_COCO_BODY_PARTS[7], v.y) >= (POSE_COCO_BODY_PARTS[0], v.y):   
+            #     hail_taxi(image)
+
+            # print([(POSE_COCO_BODY_PARTS[4], v.y) for v in human.body_parts.items()])
+            # for x in body_parts.values()
+            # print(x)
+            # print([(POSE_COCO_BODY_PARTS[0], v.y) for v in human.body_parts.items()])
+
+            # print(POSE_COCO_BODY_PARTS[0].y for v human.body_parts.values())
+            # print(human.body_parts[0].v[y])
+            # print([(POSE_COCO_BODY_PARTS[0:"Nose"], v.y) for v in human.body_parts.items()])
+            # 'unhashable type; 'slice' --> What!?
+
+        #  Evidence of bla bla bla - Stef
+
+        #- video tutorial series YT (currently watching now) https://www.youtube.com/watch?v=nUjGLjOmF7o
+        #- Theory: idealy have hail taxi when hand Rwrist and Lwrist (4, 7) are above shoulder Rsholder and Lshoulder (2,5) (for starters, start simple)
+        #- to increase complexity cancel when both hands are up + get elbow involved
+        #- using similar logic to Scarlett we can also then assume that when the wrist is past the elbow along the x axis it signifies a hail taxi (in conjunction with y value)
+
+            # if (POSE_COCO_BODY_PARTS[4], v.x) <= (POSE_COCO_BODY_PARTS[3], v.x) for v in human.body_parts.items():
+            # (POSE_COCO_BODY_PARTS[7], v.x) >= (POSE_COCO_BODY_PARTS[6], v.x):   
+
+        #more info to continue
+
+    
+            
+            # ******************** THE IMPORTANT CHANGES ****************************
+
+            # allpairs = human.body_parts[0].y #How to get the individual y coordinate value
+            #print(allpairs) #Refer to the POSE_COCO_BODY_PARTS keys for the numbers for each respective body part
+            # 0 = Nose (eventually I want to code in recognition for which eye is higher)
+            # 4 = RWrist
+            # 7 = LWrist
+            # the 'y' value takes the top of the screen as 0 and the bottom of the screen as 1.
+            # So when coding the if statements for hailing a taxi, the higher body part will have a lower value
+
+
+            # Debugging statement: remove before demonstration.
+        # print([(POSE_COCO_BODY_PARTS[k], v.x, v.y) for k,v in human.body_parts.items()])
+
+            RWristY = 1
+            RWristX = 1
+            LWristY = 1
+            LWristX = 1
+            REyeY = 1
+            LEyeY = 1
+            MaxEyeY = 1
+            RElbowX = 1
+            LElbowX = 1
+            RShoulderY = 1
+            LShoulderY = 1
+
+            for i in human.body_parts:
+                if i == 2:
+                    RShoulderY = human.body_parts[2].y
+                if i == 3:
+                    RElbowX = human.body_parts[3].x
+                if i == 4:
+                    RWristY = human.body_parts[4].y
+                    RWristX = human.body_parts[4].x
+                if i == 5:
+                    LShoulderY = human.body_parts[5].y
+                if i == 6:
+                    LElbowX = human.body_parts[6].x
+                if i == 7:
+                    LWristY = human.body_parts[7].y
+                    LWristX = human.body_parts[7].x
+                if i == 14:
+                    REyeY = human.body_parts[14].y
+                if i == 15:
+                    LEyeY = human.body_parts[15].y
+                # considered try test but this is better because you
+                # can look at all the elements in there and grab the ones you need
+                # try/exception testing would be bulkier
+            if REyeY <= LEyeY:
+                MaxEyeY = REyeY
+            else:
+                MaxEyeY = LEyeY
+            # print ("MaxEyeY: " + str(MaxEyeY))
+            if LWristY <= MaxEyeY and LElbowX <= LWristX and LWristX != 1:
+                hail_taxi(image)
+                print("Someone is hailing a LEFT taxi!")
+                # print("LWristY: " + str(LWristY))
+                # print("LWristX: " + str(LWristX))
+                # print("LElbowX: " + str(LElbowX))
+            if RWristY <= MaxEyeY and RElbowX >= RWristX:
+                hail_taxi(image)
+                print("Someone is hailing a RIGHT taxi!")
+                # print("RWristY: " + str(RWristY))
+                # print("RWristX: " + str(RWristX))
+                # print("RElbowX: " + str(RElbowX))
+
+            # if human.body_parts[4] == None:
+            #     RWrist = 0
+            # else:
+            #     RwristY = human.body_parts[4].y
+            #     RWristX = human.body_parts[4].x
+
+            # if human.body_parts[0].y = None:
+            #     Nose = human.body_parts[0].y
+            # if human.body_parts[0].y
+            # Nose = 
+            # print(Nose)
+            # Rwrist = human.body_parts[4].y
+            # print(Rwrist)
+            # if Rwrist <= Nose:
+            #     print("Someone is hailing a taxi!")
+
+
+        # drawing lines on an image
+        image = TfPoseEstimator.draw_humans(image, humans, imgcopy=False)
+
+        # FPS counter
+        cv2.putText(image, "FPS: {:.2f}".format(1.0 / (time.time() - fps_time)),
+                    (10, 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (128, 0, 0), 2)
+        cv2.imshow('tf-pose-estimation result', image)
+        fps_time = time.time()
+        if cv2.waitKey(1) == 27:
+            break
+
+    cv2.destroyAllWindows()
--- week7/readme.md
+++ week7/readme.md
@@ -1,1 +1,16 @@
 TODO: Reflect on what you learned this week and what is still unclear.
+
+
+Jupyter stuff
+
+penalty_data.columns
+displays all columns, but as an index
+
+list(penalty_data.columns)
+displays all columns as a list
+
+indexes are usually better, but occassionally a list is required
+
+row_one = penalty_data.loc[1] --> or .iloc works as well
+That gives you the row of the given index
+
